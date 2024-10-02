719b52adeb9b94852c71f402b0b3f1c8eaffff75
Amine-youcode <bourhymamine@gmail.com>
2024-10-02 15:43:34 +0200

    les strigue

---
+++ strig1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char T[100];
+    puts("entre une chain \n");
+    gets(T);
+    puts("la chaine est ");
+    puts(T);
+
+    return 0;
+}
